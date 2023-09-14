228d36435e047b31a2a75f968604090bf0850207
Luis Lechuga Ruiz <luislechugaruiz@gmail.com>
2023-09-14 22:32:08 +0200

    Fix installation

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     name='act',
     version='0.0.1',
     packages=find_packages(),
+    package_data={
+        'act': ['config/*.json']
+    },
     license='MIT License',
     # long_description=open('README.md').read(),
 )
